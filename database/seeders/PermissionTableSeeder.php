ebfcaee3ceb88c45319ee0acf3139dab2690705f
erylestari <erylestari46@gmail.com>
2023-10-08 15:23:42 +0700

    create mvc

---
+++ database/seeders/PermissionTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Spatie\Permission\Models\Permission;
+
+class PermissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Permissions
+        $permissions = [
+            'role-list',
+            'role-create',
+            'role-edit',
+            'role-delete',
+            'role-show',
+            'product-list',
+            'product-create',
+            'product-edit',
+            'product-delete',
+            'product-show'
+        ];
+       
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+    }
+}
